518ddbdad7236115ab9ace7b0e498838b535e725
RaveHunter05 <paulsotelor@yahoo.es>
2025-10-02 00:17:37 -0600

    feat: initial models for vector database and get db context

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -1,6 +1,22 @@
+namespace bot_messenger.Context;
+
 using Microsoft.EntityFrameworkCore;
+
+using bot_messenger.Models;
 
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<DocumentEmbedding> DocumentEmbeddings { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<DocumentEmbedding>(entity =>
+                {
+                    entity.HasKey(e => e.Id);
+                    entity.Property(e => e.Embedding)
+                          .HasColumnType("vector(1536)");
+                });
+    }
 }
---
+++ Models/DocumentEmbedding.cs
@@ -0,0 +1,10 @@
+namespace bot_messenger.Models;
+
+public class DocumentEmbedding
+{
+    public int Id { get; set; }
+    public string Content { get; set; }
+    public float[] Embedding { get; set; } // Se mapea al tipo vector
+    public string Metadata { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
